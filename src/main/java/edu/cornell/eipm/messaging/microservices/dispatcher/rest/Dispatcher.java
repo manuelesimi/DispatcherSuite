fbd74b56fdf2cc206302a135e6fd0da193644c3f
Manuele Simi <mas2182@med.cornell.edu>
2019-09-27 16:42:38 -0400

    Parse the configuration in the PostConstruct step.

--- src/main/java/edu/cornell/eipm/messaging/microservices/dispatcher/rest/Dispatcher.java
+++ src/main/java/edu/cornell/eipm/messaging/microservices/dispatcher/rest/Dispatcher.java
@@ -3,6 +3,9 @@ package edu.cornell.eipm.messaging.microservices.dispatcher.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
 /**
  * Main app for Dispatcher.
  *
@@ -13,5 +16,10 @@ public class Dispatcher {
 
     public static void main(String[] args){
         SpringApplication.run(Dispatcher.class, args);
+    }
+
+    @PostConstruct
+    private void init() throws IOException {
+        ConfigParser.parse();
     }
 }
