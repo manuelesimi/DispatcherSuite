0fc7aa644382ef0b1418497975b57130d5b6a848
Manuele Simi <mas2182@med.cornell.edu>
2019-10-16 16:37:08 -0400

    Use ConsumerRecord to access the message and its topic.

--- src/main/java/edu/cornell/eipm/messaging/microservices/dispatcher/broker/consumer/Receiver.java
+++ src/main/java/edu/cornell/eipm/messaging/microservices/dispatcher/broker/consumer/Receiver.java
@@ -1,10 +1,12 @@
 package edu.cornell.eipm.messaging.microservices.dispatcher.broker.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -27,8 +29,8 @@ public class Receiver {
   }
 
   @KafkaListener(topics = "#{'${kafka.consumer.topics}'.split(',')}")
-  public void receive(String payload) {
-    LOGGER.info("received payload='{}'", payload);
-    latch.countDown();
+  public void receive(ConsumerRecord<?, Map<String, String>> message) {
+      LOGGER.info("Received messages on topic [{}]: [{}]", message.topic(), message.value());
+      latch.countDown();
   }
 }
