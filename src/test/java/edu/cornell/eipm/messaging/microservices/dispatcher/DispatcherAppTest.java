5059d95578ae59e6a94dc2708d000b0c7987cd77
Manuele Simi <mas2182@med.cornell.edu>
2019-10-16 22:49:02 -0400

    Fix Boot tests.

--- src/test/java/edu/cornell/eipm/messaging/microservices/dispatcher/DispatcherAppTest.java
+++ src/test/java/edu/cornell/eipm/messaging/microservices/dispatcher/DispatcherAppTest.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import edu.cornell.eipm.messaging.microservices.dispatcher.DispatcherApp;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
