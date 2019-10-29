dd12ccc052c61ff39317ecbd2ed85264886a0b3d
Manuele Simi <mas2182@med.cornell.edu>
2019-10-29 16:45:19 -0400

    First docker version.

---
+++ buildDockerImage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+docker_image_name='eipm/kafka-dispatcher'
+docker_image_tag='1.0'
+docker build --iidfile ./id.txt .
+docker tag $(cat ./id.txt) ${docker_image_name}:${docker_image_tag}
+docker tag $(cat ./id.txt) ${docker_image_name}:latest
+rm ./id.txt
