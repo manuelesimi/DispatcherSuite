956ea42aa1ec64c67229dd926f7f1f7c0008cb87
Manuele Simi <mas2182@med.cornell.edu>
2019-12-04 12:55:20 -0500

    Create 3 crons to execute lists of configurable triggers. Update yaml configuration.

--- src/main/java/edu/cornell/eipm/messaging/microservices/dispatcher/jobs/Scheduler.java
+++ src/main/java/edu/cornell/eipm/messaging/microservices/dispatcher/jobs/Scheduler.java
@@ -2,6 +2,7 @@ package edu.cornell.eipm.messaging.microservices.dispatcher.jobs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
@@ -12,16 +13,33 @@ import java.util.Date;
  *
  * @author Manuele Simi
  */
+@Configuration
 @Component
 public class Scheduler {
 
     private final Logger logger = LoggerFactory.getLogger(Scheduler.class);
 
-    @Scheduled(fixedDelay = 10000)
-    public void cronJob() throws Exception {
+    @Scheduled(fixedRateString = "#{'${scheduler.cron1.rate}'}")
+    public void cronJob1() throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         Date now = new Date();
         String strDate = sdf.format(now);
-        logger.info("Java cron job started at: " + strDate);
+        logger.info("Java cron1 job started at: " + strDate);
+    }
+
+    @Scheduled(fixedRateString = "#{'${scheduler.cron2.rate}'}")
+    public void cronJob2() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        logger.info("Java cron2 job started at: " + strDate);
+    }
+
+    @Scheduled(fixedRateString = "#{'${scheduler.cron3.rate}'}")
+    public void cronJob3() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        logger.info("Java cron3 job started at: " + strDate);
     }
 }
