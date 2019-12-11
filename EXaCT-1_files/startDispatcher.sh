0858be713b3155a036506f379c300986239f5c36
Manuele Simi <mas2182@med.cornell.edu>
2019-12-11 15:40:55 -0500

    Update submission and config files for EXaCT-1.

--- EXaCT-1_files/startDispatcher.sh
+++ EXaCT-1_files/startDispatcher.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-CONFIG_FILE=/home/mas2182/application.yml
+CONFIG_FILE=/home/mas2182/EXaCT-1/application.yml
 echo -e "Using config file at ${CONFIG_FILE}"
 
-docker run -p 8080:8080 --rm --network host \
+docker run -p 80:80 --rm --network host \
     -e HOST_HOSTNAME=`hostname` \
     -e HOST_USER=$LOGNAME \
     -v /home/mas2182/.ssh/:/ssh/ -v $CONFIG_FILE:/config/application.yml eipm/kafka-dispatcher:1.0
-startDispatcher.sh (END)
