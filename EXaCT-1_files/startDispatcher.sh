b0b7eb13c93ea5703488c596e58edf8079b877ae
Manuele Simi <mas2182@med.cornell.edu>
2019-12-18 23:15:26 -0500

    Remove test files.

--- EXaCT-1_files/startDispatcher.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CONFIG_FILE=/home/mas2182/EXaCT-1/application.yml
-echo -e "Using model file at ${CONFIG_FILE}"
-
-docker run -p 80:80 --rm --network host \
-    -e HOST_HOSTNAME=`hostname` \
-    -e HOST_USER=$LOGNAME \
-    -v /home/mas2182/.ssh/:/ssh/ -v $CONFIG_FILE:/config/application.yml eipm/kafka-dispatcher:1.0
