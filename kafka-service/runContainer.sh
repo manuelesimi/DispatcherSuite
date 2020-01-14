9bd941c9ff9daa10479799afbc87757f83ad6bbf
Manuele Simi <mas2182@med.cornell.edu>
2020-01-14 11:27:17 -0500

    Use parenthesis instead of backticks.

--- kafka-service/runContainer.sh
+++ kafka-service/runContainer.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -x
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -p 8080:8080 --rm \
- -e HOST_HOSTNAME=`hostname` \
+ -e HOST_HOSTNAME=$(hostname) \
  -e HOST_USER=$LOGNAME \
  -v "${WORKING_DIR}/application.yml":/application.yml \
  eipm/kafka-dispatcher:1.1
--- kafka-service/runSSLContainer.sh
+++ kafka-service/runSSLContainer.sh
@@ -3,7 +3,7 @@
 WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -p 8443:8443 --rm  \
- -e HOST_HOSTNAME=`hostname` \
+ -e HOST_HOSTNAME=$(hostname) \
  -e HOST_USER=$LOGNAME \
  -v "${WORKING_DIR}/application-ssl.yml":/application.yml \
  eipm/kafka-dispatcher:1.1
