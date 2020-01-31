2b38b34266db792979081cdc1015e75421b35a99
Manuele Simi <mas2182@med.cornell.edu>
2020-01-31 11:42:59 -0500

    Restore kafka-embedded test cases.

--- kafka-service/src/test/java/edu/cornell/eipm/messaging/microservices/kafka/dispatcher/DispatcherControllerTest.java
+++ kafka-service/src/test/java/edu/cornell/eipm/messaging/microservices/kafka/dispatcher/DispatcherControllerTest.java
@@ -10,13 +10,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DispatcherApp.class)
 @AutoConfigureMockMvc
-public class DispatcherAppTest {
+@EmbeddedKafka(partitions = 1,
+        topics = {SpringKafkaApplicationTest.TEST_TOPIC})
+public class DispatcherControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
